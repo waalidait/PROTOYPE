5d8c3888e749eee383e9f67541c2bba1b5b867d6
waalidait <snwalid08@gmail.com>
2025-09-25 10:28:16 +0100

    ajouter un commantaire dans le fichier

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
- 
-
-  function prototyp(list,word){
-    let index = 0;
-    let count = 0;
-    while(index < list.length){
-      if(word===list[index]){
-        count++
-      }   
-      index++
+function prototype(list , word){
+  let index = 0;
+  let count = 0;
+  while(index < list.length){
+    if(word===list[index]){
+      count++
     }
-    if (count == 0) {
-       alert("Le mot '" + word + "' n'existe pas dans la liste.");
-    }
-    
-      return count ;
+    index++
   }
-
-  let list = ["C1","C2","C3","C3","C1","C2","C1"];
-  let word = prompt("ecri le code");
-  let count =prototyp(list,word);
-
-    if(count > 0){
-       alert(word + "   est repet  " + count +"  fois");
-    }
-
+  if(count==0){
+    alert("ce "+word+" n'est pas existe dans la list");
+  }
+  return count;
+}                
+  let list =  ["C1","C2","C3","C3","C1","C2","C1"]; /*the list and the fuction are not the same*/
+                 
+  let word = prompt("entre "); /*the word and the fuction are not the same */
+  let count = prototype(b,a);/*the count and the fuction are not the same */
   
+  if(count > 0){
+    alert(word + "est repete "+count + " fois");
+  }
